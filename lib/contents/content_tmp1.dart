6a2a2131e0a99adad3e9c84df9c05f4944d4bcb3
wontae <bbrick@bbrickui-MacBookPro.local>
2022-09-13 16:50:50 +0900

    first commit

---
+++ lib/contents/content_tmp1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import "package:flutter/material.dart";
+
+class Tmp1Content extends StatelessWidget {
+  const Tmp1Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Tmp1"),
+    );
+  }
+}
---
+++ lib/screen/index.dart
@@ -0,0 +1,64 @@
+import "package:flutter/material.dart";
+import 'package:dashboard/contents/content_balance.dart';
+import 'package:dashboard/contents/content_tmp1.dart';
+import 'package:dashboard/contents/content_tmp2.dart';
+
+class IndexScreen extends StatefulWidget {
+  const IndexScreen({super.key});
+
+  @override
+  State<IndexScreen> createState() => _IndexScreenState();
+}
+
+class _IndexScreenState extends State<IndexScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _mainContents = [
+    BalanceContent(),
+    Tmp1Content(),
+    Tmp2Content()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          NavigationRail(
+            destinations: const <NavigationRailDestination>[
+              NavigationRailDestination(
+                  icon: Icon(IconData(0xf1dd, fontFamily: 'MaterialIcons')),
+                  // on 0xe3f7
+                  label: Text("Balance")),
+              NavigationRailDestination(
+                  icon: Icon(IconData(0xe042, fontFamily: 'MaterialIcons')),
+                  // on 0xe3f7
+                  label: Text("Tmp1")),
+              NavigationRailDestination(
+                  icon: Icon(IconData(0xe74a, fontFamily: 'MaterialIcons')),
+                  // on 0xe3f7
+                  label: Text("Tmp2")),
+            ],
+            selectedIndex: _currentIndex,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            labelType: NavigationRailLabelType.all,
+            elevation: 5,
+            indicatorColor: Colors.cyan[50],
+          ),
+          const VerticalDivider(
+            thickness: 5,
+            width: 1,
+          ),
+          Expanded(
+            child: Container(
+                padding: EdgeInsets.all(30),
+                child: _mainContents[_currentIndex]),
+          )
+        ],
+      ),
+    );
+  }
+}
